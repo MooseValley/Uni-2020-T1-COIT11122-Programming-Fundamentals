61dd5b4b4505b524e6aa02fa3342d22daaa5375c
MooseValley <Moose_Software@yahoo.com.au>
2020-03-11 12:49:58 +1000

    Week 01 update

---
+++ Week 01/First.java
@@ -0,0 +1,31 @@
+/*
+  Author: Mike O
+  Source: First.java
+  Descr:  My first Java program, produces console output.
+
+*/
+
+
+/*
+javac First.java
+java First
+
+*/
+// Comment
+public class First
+{
+	public static void main(String[] args)
+	{
+		System.out.println ("Hello");
+
+		System.out.println ("akljalkdjaldjalkajlksd"); // Comment here
+
+		// Set age to 3.
+		/*
+		int age = 3; // Set age to 3.
+
+		if (age < 10)
+		   System.out.println ("You are over 20"); // Logic Error
+		*/
+	}
+}
---
+++ Week 01/SwingDialogs.java
@@ -0,0 +1,14 @@
+
+//import javax.swing.JOptionPane;
+import javax.swing.*;
+
+public class SwingDialogs
+{
+   public static void main(String[] args)
+   {
+      JOptionPane.showMessageDialog (null, "Hello World!");
+
+      JButton calc = new JButton ("Calc");
+
+   }
+}
