b66dafca9ee651139f410f1484797a48034dc8eb
MooseValley <Moose_Software@yahoo.com.au>
2020-04-23 20:52:02 +1000

    Week 06

---
+++ Week 06/SimpleMethodExamplesFromLecture.java
@@ -0,0 +1,36 @@
+public class SimpleMethodExamplesFromLecture
+{
+   public static void simpleMethod (double d)
+   {
+      System.out.println ("method takes a double: " + d);
+   }
+
+   public static void simpleMethod (String s)
+   {
+      System.out.println ("method takes a String: " + s);
+   }
+
+   public static void simpleMethod (boolean b)
+   {
+      System.out.println ("method takes a boolean: " + b);
+   }
+
+   public static void main (String[] args)
+   {
+      // Overloading / Polymorphism in action:
+      simpleMethod ("Mike");
+      simpleMethod (45.45);
+      simpleMethod (true);
+      simpleMethod (45);
+      simpleMethod ('A'); // Which method ?
+
+      //int, long, short, byte, char = 65 'A'
+
+      char charA1 = 'A';
+      System.out.println (charA1);  // A
+
+      char charA2 = 65;
+      System.out.println (charA2);  // A
+   }
+
+}
