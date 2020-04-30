86f2e8714706b1377a8bb2ded5a79ccd2bb66563
MooseValley <Moose_Software@yahoo.com.au>
2020-04-30 20:53:54 +1000

    Week 07

---
+++ Week 07/ArrayMethodDemo.java
@@ -0,0 +1,7 @@
+/*
+4. Create an application containing an array that stores 10 integers. The application
+should call five methods that in turn (1) display all the integers, (2) display all the
+integers in reverse order, (3) display the sum of the integers, (4) display all values less
+than a limiting argument, and (5) display all values that are higher than the calculated
+average value. Save the file as ArrayMethodDemo.java.
+*/
---
+++ Week 07/TwelveInts.java
@@ -0,0 +1,5 @@
+/*
+1. Write an application that stores 12 integers in an array. Display the integers from
+first to last, and then display the integers from last to first. Save the file as
+TwelveInts.java.
+*/
