36fb1204c43afccd920408cf2337ecc9ae281e30
MooseValley <Moose_Software@yahoo.com.au>
2020-05-06 13:43:26 +1000

    Week 08

--- Week 07/Employee.java
+++ Week 07/Employee.java
@@ -2,9 +2,41 @@
  Author: Mike O
  File:   Employee.java
  Desc:   Basic class for an Employee.
+
+UML Class Diagram
+short summary of your class
+
+-----------------------------------
+Employee
+-----------------------------------
+-<static>MAX_ID: int, 999
+-id: int
+-name: String
+-traininghistory: TrainingHistory[]
+-----------------------------------
+<<constructor>>Employee()
+<<constructor>>Employee (String name)
+<<constructor>>Employee (int id)
+<<constructor>>Employee (String name, int id)
++setId (int id): void
++setName (String name): void
++getId (): int
++getName (): String
++toString (): String
+-----------------------------------
+
+Key:
+- private
++ public
+<<constructor>> Constructor
+<static> Static class methods / fields
+
+
 */
 public class Employee
 {
+   private static final int MAX_ID = 999;
+
    private int    id;
    private String name;
 
