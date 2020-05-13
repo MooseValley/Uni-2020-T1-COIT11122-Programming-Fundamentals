c42faba07cc131e2394b384c350217e246f55ced
MooseValley <Moose_Software@yahoo.com.au>
2020-05-13 16:07:27 +1000

    Week 09

---
+++ Week 09/LectureGuiExample - v0.01 - template/LectureGuiExample.java
@@ -0,0 +1,25 @@
+/*
+Author: Mike OMalley
+Source: LectureGuiExample.java
+Desc:   Develop a basic GUI user interface using the components as
+        we cover them in the lecture class.
+*/
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LectureGuiExample
+{
+   // Constants:
+
+   // GUI Components:
+
+   // Class instance data:
+
+   // Constructor / Methods:
+
+
+   public static void main (String[] args)
+   {
+   }
+}
